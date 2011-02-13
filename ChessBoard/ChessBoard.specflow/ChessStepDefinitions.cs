eeb576f0d7e76d6a0285ca017a38b3b3f1e4d7f0
TimSharrock <tim@sharrock.org.uk>
2011-02-13 18:35:12 +0000

    First test passing
    the response is always illegal move...
    not a lot to refactor yet

---
+++ ChessBoard/ChessBoard.specflow/ChessStepDefinitions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessBoard.specflow
+{
+    [Binding]
+    public class ChessStepDefinitions
+    {
+        ChessBoardLib.Game myGame;
+
+        public ChessStepDefinitions()
+        {
+            myGame = new ChessBoardLib.Game();
+        }
+
+        [Given(@"I have a White Pawn at A8")]
+        public void GivenIHaveAWhitePawnAtA8()
+        {
+            myGame.addPawnAtA8();
+        }
+
+        [Given(@"I have a Black Knight at A1")]
+        public void GivenIHaveABlackKnightAtA1()
+        {
+            myGame.addKnightAtA1();
+        }
+
+        [When(@"I move the Pawn to A9")]
+        public void WhenIMoveThePawnToA9()
+        {
+            myGame.movePawnToA9();
+        }
+
+        [Then(@"I should be warned of an illegal move message")]
+        public void ThenIShouldBeWarnedOfAnIllegalMoveMessage()
+        {
+            Assert.AreEqual("Illegal move",myGame.response());
+        }
+    }
+}
---
+++ ChessBoard/ChessBoardLib/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessBoardLib
+{
+    public class Game
+    {
+        public void addPawnAtA8()
+        {
+            // don't do anything yet...
+        }
+
+        public void movePawnToA9()
+        {
+            // nada
+        }
+
+        public void addKnightAtA1()
+        {
+            // do nothing yet
+        }
+
+        public string response()
+        {
+            return "Illegal move";
+        }
+    }
+}
